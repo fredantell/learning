226b234de4694bc38f544f85063389ca822959bd
Fredrik Antell <fred.antell@gmail.com>
2013-09-20 20:35:07 -0500

    Add routing using req.params.var and page builders

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ app.use(express.logger());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-	response.send('Hello Worlds');
+// app.get('/', function(request, response) {
+//  response.send('Hello Worlds');
+// });
+
+app.get('/', function(req, res) {
+  res.send(pages.createPage('index'));
 });
 
-app.get('/fredrik', function(request, response) {
-	response.send(pages.fredrik());
+app.get('/:pageName', function(req, res) {
+  var pageName = req.params.pageName;
+  res.send(pages.createPage(pageName));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
 
--- pages.js
+++ pages.js
@@ -1,17 +1,28 @@
-var buildData = function() {
-  var data = require('./data.json');
-  var html = '' +
-  '<div>' +
-  JSON.stringify(data) +
-  '</div>';
-  return html;
-};
+// var buildData = function() {
+//   var data = require('./data.json');
+//   var html = '' +
+//   '<div>' +
+//   JSON.stringify(data) +
+//   '</div>';
+//   return html;
+// };
 
-var buildHead = function() {
+//-------------------------------
+//----Builders for page sections-
+//-------------------------------
+
+var buildHeader = function(pageTitle) {
+  pageTitle = pageTitle || '';
+
   var html = '' +
   '<html>' +
   '<head>' +
-  '<body>';
+  '<meta charset="UTF-8" />' +
+  '<title>Roux - ' + pageTitle + '</title>' +
+  "<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>" +
+  "<link href='./lib/bootstrap/css/bootstrap.css' rel='stylesheet'>" +
+  '</head>' +
+  '<body id="' + pageTitle + '">';
 
   return html;
 };
@@ -25,12 +36,77 @@ var buildFooter = function() {
   return html;
 };
 
-
-exports.fredrik = function() {
+var buildBodyIndex = function() {
   var html = '' +
-  buildHead() +
-  buildData() +
+  '<section class="container">' +
+  '  <div class="content row">' +
+  '    <section class="main col col-lg-8 col-md-6">' +
+  '      <h2>Main Content</h2>' +
+  '      <p>' + loremText +
+  '      </p>' +
+  '    </section><!-- main -->' +
+  '    <section class="sidebar col col-lg-2 col-md-6">' +
+  '      <h2>Sidebar</h2>' +
+  '      <p>' + loremText +
+  '      </p>' +
+  '    </section><!-- sidebar -->' +
+  '  </div><!-- content -->' +
+  '</section><!-- container -->';
+
+  return html;
+
+};
+
+
+
+//-------------------------------
+//----Builders for whole pages---
+//-------------------------------
+
+var buildPageIndex = function() {
+  var html = '' +
+  buildHeader('index') +
+  buildBodyIndex() +
   buildFooter();
 
   return html;
 };
+var buildPage404 = function() {
+  var html = '' +
+  buildHeader('404 page not found') +
+  '<div style="font-size:40px; text-align:center; margin-top:50px;">' +
+  'Sorry this page does not exist</div>' +
+  buildFooter();
+
+  return html;
+};
+var buildPageFredrik = function() {
+  var html = '' +
+  buildHeader('404 page not found') +
+  '<div style="font-size:40px; text-align:center; margin-top:50px;">' +
+  'Wowaweewa!  You got it working!</div>' +
+  buildFooter();
+
+  return html;
+};
+
+//-------------------------------
+//----Exports Section------------
+//-------------------------------
+
+exports.createPage = function(key) {
+  var pageFns = {
+    index: buildPageIndex,
+    fredrik: buildPageFredrik
+  };
+
+  if (pageFns[key] === undefined) {
+    return buildPage404();
+  }
+
+  return pageFns[key]();
+};
+
+
+
+var loremText = 'Wowaweewa!  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
