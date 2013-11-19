083065ae81ceb2a6d18f08f7a6b291aa7219a87e
Fredrik Antell <fred.antell@gmail.com>
2013-11-19 01:08:03 -0600

    Fix route for app.js

--- JSEvents/app.js
+++ JSEvents/app.js
@@ -7,7 +7,9 @@ app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 
-
+app.get('/', function(req, res) {
+  res.redirect('/arteliminator');
+});
 app.get('/:pageName', function(req, res) {
   console.log(req.params.pageName);
   res.send(pages.createPage(req.params.pageName));
