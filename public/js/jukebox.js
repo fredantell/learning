ec90f21f591da3a581a3ffbe6e6248e0fc1452dd
Fredrik Antell <fred.antell@gmail.com>
2013-11-18 23:15:35 -0600

    Add files for jukebox project

---
+++ public/js/jukebox.js
@@ -0,0 +1,18 @@
+
+module.exports = function() {
+  var html = '' +
+      '' +
+     '<div class="jukeboxContainer">' +
+       '<div class="jukebox">' +
+         '<ul class="tracklist">' +
+           '<li class="" data-src="audio/AmazingLee.mp3">Amazing</li>' +
+           '<li class="" data-src="audio/EqueKenox.mp3">EqueKnox</li>' +
+           '<li class="" data-src="audio/NightKitty.mp3">Night Kitty</li>' +
+           '<li class="" data-src="audio/Phoebex.mp3">Phoebex</li>' +
+           '<li class="" data-src="audio/Shiloah.mp3">Shiloah</li>' +
+         '</ul>' +
+       '</div>' + //close div.jukebox
+     '</div>'; //close div.jukeboxContainer
+
+  return html;
+};
