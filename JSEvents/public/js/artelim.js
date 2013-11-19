c707d59c94229e5bd70d6c7658989ff5efeb9f24
Fredrik Antell <fred.antell@gmail.com>
2013-11-19 01:32:47 -0600

    Fix case sensitivity in LaRue for artwork var

--- JSEvents/public/js/artelim.js
+++ JSEvents/public/js/artelim.js
@@ -72,14 +72,14 @@ var artwork = {
     },
     {
       "artist": "LaVonne LaRue",
-      "filename": "Lavonne_Larue_02.jpg",
-      "thumbnail": "Lavonne_Larue_02_tn.jpg",
+      "filename": "Lavonne_LaRue_02.jpg",
+      "thumbnail": "Lavonne_LaRue_02_tn.jpg",
       "title": "Graffiti on Brick"
     },
     {
       "artist": "LaVonne LaRue",
-      "filename": "Lavonne_Larue_04.jpg",
-      "thumbnail": "Lavonne_Larue_04_tn.jpg",
+      "filename": "Lavonne_LaRue_04.jpg",
+      "thumbnail": "Lavonne_LaRue_04_tn.jpg",
       "title": "Village Oasis"
     },
     {
